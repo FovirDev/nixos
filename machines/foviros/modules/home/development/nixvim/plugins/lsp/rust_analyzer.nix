30880b4b66397f9aa2f9c1621ad6d2fb0b5be55b
Fovir <fovir@disroot.org>
2026-07-11 01:52:43 +0800

    feat: set `clippy` as check command
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/development/nixvim/plugins/lsp/rust_analyzer.nix
+++ machines/foviros/modules/home/development/nixvim/plugins/lsp/rust_analyzer.nix
@@ -1,10 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
+  extraPackages = [ pkgs.clippy ];
+
   plugins.lsp.servers.rust_analyzer = {
     enable = true;
 
     installCargo = false;
     installRustc = false;
     installRustfmt = false;
-    settings.cargo.allFeatures = true;
+    settings = {
+      cargo.allFeatures = true;
+      check.command = "clippy";
+    };
   };
 }
