8ce7dfda039e6c8aac8a515c5eb7fbcf747d22f6
Fovir <fovir@disroot.org>
2026-07-20 18:45:10 +0800

    feat(zsh): add vim mode cursor change
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/terminal/zsh.nix
+++ machines/foviros/modules/home/terminal/zsh.nix
@@ -62,6 +62,23 @@ in
       set -o vi
       bindkey -M viins '^H' backward-kill-word
 
+      function zle-keymap-select {
+        if [[ ''${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+          echo -ne '\e[1 q'
+        elif [[ ''${KEYMAP} == main ]] || [[ ''${KEYMAP} == viins ]] || [[ ''${KEYMAP} = "" ]] || [[ $1 = 'beam' ]]; then
+          echo -ne '\e[5 q'
+        fi
+      }
+      zle -N zle-keymap-select
+
+      zle-line-init() {
+          zle -K viins
+          echo -ne "\e[5 q"
+      }
+      zle -N zle-line-init
+
+      echo -ne '\e[5 q'
+
       bgnotify_threshold=10
       function bgnotify_formatted {
         ## $1=exit_status, $2=command, $3=elapsed_time
