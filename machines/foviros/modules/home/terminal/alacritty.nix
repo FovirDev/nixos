a48ea9249da27edb53f3332bb18d1c70d694c225
Fovir <fovir@disroot.org>
2026-07-20 20:08:54 +0800

    feat: configure `alacritty`
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/terminal/alacritty.nix
+++ machines/foviros/modules/home/terminal/alacritty.nix
@@ -3,9 +3,16 @@
     enable = true;
     theme = "modus_vivendi";
     settings = {
+      env = {
+        TERM = "xterm-256color";
+      };
       font = {
         normal.family = "JetBrainsMono Nerd Font Mono";
         size = 12;
+      };
+      window = {
+        dynamic_padding = true;
+        decorations = "None";
       };
     };
   };
