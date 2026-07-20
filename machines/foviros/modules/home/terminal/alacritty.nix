8b9a904cb5aa204819ea1cb9035b5bb83c86b728
Fovir <fovir@disroot.org>
2026-07-20 20:16:52 +0800

    feat: configure vi mode in `alacritty`
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/terminal/alacritty.nix
+++ machines/foviros/modules/home/terminal/alacritty.nix
@@ -3,12 +3,33 @@
     enable = true;
     theme = "modus_vivendi";
     settings = {
+      cursor = {
+        vi_mode_style = {
+          shape = "Block";
+          blinking = "Off";
+        };
+      };
       env = {
         TERM = "xterm-256color";
       };
       font = {
         normal.family = "JetBrainsMono Nerd Font Mono";
         size = 12;
+      };
+      keyboard = {
+        bindings = [
+          {
+            action = "ToggleViMode";
+            key = "Escape";
+            mods = "Alt";
+          }
+          {
+            action = "ToggleViMode";
+            key = "A";
+            mode = "Vi";
+            mods = "Shift";
+          }
+        ];
       };
       window = {
         dynamic_padding = true;
