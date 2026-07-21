ba78d7744d42fbca566ce4b2b5e488dd7d60a1b8
Fovir <fovir@disroot.org>
2026-07-21 15:20:44 +0800

    fix: unset `WAYLAND_DISPLAY` to execute `oculante`
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/media/oculante.nix
+++ machines/foviros/modules/home/media/oculante.nix
@@ -1,3 +1,57 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.oculante ];
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  home = {
+    file.".local/bin/run-oculante" = {
+      executable = true;
+      text = ''
+        #!/usr/bin/env bash
+
+        env -u WAYLAND_DISPLAY oculante $1
+      '';
+    };
+    packages = [ pkgs.oculante ];
+  };
+  xdg.desktopEntries.oculante = {
+    exec = "${config.home.homeDirectory}/.local/bin/run-oculante";
+    icon = "oculante";
+    name = "Oculante";
+    genericName = "Image Viewer";
+    mimeType = [
+      "image/bmp"
+      "image/gif"
+      "image/vnd.microsoft.icon"
+      "image/jpeg"
+      "image/jp2"
+      "image/png"
+      "image/pnm"
+      "image/x-tga"
+      "image/jxl"
+      "image/avif"
+      "image/tiff"
+      "image/webp"
+      "image/octet-stream"
+      "application/dicom"
+      "application/vnd.adobe.photoshop"
+      "image/svg+xml"
+      "image/exr"
+      "image/x-exr"
+      "image/x-dcraw"
+      "image/x-nikon-nef"
+      "image/x-canon-cr2"
+      "image/x-adobe-dng"
+      "image/x-epson-erf"
+      "image/x-fuji-raf"
+      "image/x-sony-arw"
+      "image/x-sony-srf"
+      "image/x-sony-sr2"
+      "image/x-panasonic-raw/x-portable-pixmap"
+      "image/heic"
+      "image/x-qoi"
+      "image/"
+    ];
+  };
 }
