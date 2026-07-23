3389adf51a7f314a89d06e261f2b0ef66fadcdb2
Fovir <fovir@disroot.org>
2026-07-23 20:41:37 +0800

    feat: update `web-devicons`
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/development/nixvim/plugins/web-devicons.nix
+++ machines/foviros/modules/home/development/nixvim/plugins/web-devicons.nix
@@ -1,3 +1,19 @@
 { ... }: {
   plugins.web-devicons.enable = true;
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      vimPlugins = prev.vimPlugins // {
+        nvim-web-devicons = prev.vimPlugins.nvim-web-devicons.overrideAttrs (_old: {
+          version = "unstable-2026-07-23";
+          src = final.fetchFromGitHub {
+            owner = "nvim-tree";
+            repo = "nvim-web-devicons";
+            rev = "2ae6958df7ced50baac5035cec0c15799eedfbf7";
+            hash = "sha256-/NTrUcytMahe5wZUjVO9a4D9vooKtAQu/cKgOugeZNk=";
+          };
+        });
+      };
+    })
+  ];
 }
