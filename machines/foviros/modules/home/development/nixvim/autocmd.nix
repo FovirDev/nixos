7ef07a613a753ab22cc1ea85a9fe5eb7d7efb5f7
Fovir <fovir@disroot.org>
2026-07-27 02:06:50 +0800

    feat: create autocmd to remove no name buffer automatically
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/development/nixvim/autocmd.nix
+++ machines/foviros/modules/home/development/nixvim/autocmd.nix
@@ -18,5 +18,27 @@
       pattern = "*";
       desc = "Automatically checktime";
     }
+    {
+      event = [ "BufEnter" ];
+      callback.__raw = ''
+        function(args)
+          local bufname = vim.api.nvim_buf_get_name(args.buf)
+          if bufname == "" then
+            return
+          end
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if buf ~= args.buf
+              and vim.api.nvim_buf_is_loaded(buf)
+              and vim.api.nvim_buf_get_name(buf) == ""
+              and not vim.bo[buf].modified
+              and vim.bo[buf].buflisted
+            then
+              vim.api.nvim_buf_delete(buf, { force = true })
+            end
+          end
+        end
+      '';
+      desc = "Automatically remove no name buffers";
+    }
   ];
 }
